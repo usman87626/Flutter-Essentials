8dca114ea9210ea327449af7ffa2d166f6562338
usman87626 <usman.arif20188@gmail.com>
2021-07-18 15:58:23 +0500

    Chapter 5 Done

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ void main() {
   runApp(MaterialApp(
     title: "My Awesome App",
     home: HomePage(),
+    theme: ThemeData(
+      primarySwatch: Colors.indigo,
+    ),
   ));
 }
 
@@ -16,8 +19,49 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        child: Center(child: Text("Hello Flutter")),
+      body: Container(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            // DrawerHeader(
+            //   child: Text(
+            //     "Welcome",
+            //     style: TextStyle(color: Colors.white),
+            //   ),
+            //   decoration: BoxDecoration(color: Colors.indigo),
+            // ),
+            UserAccountsDrawerHeader(
+                accountName: Text("HashDevelop"),
+                accountEmail: Text("hashdevelop2k20@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://i1.sndcdn.com/avatars-000909207460-hz0qjt-t500x500.jpg"),
+                )),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("My Account"),
+              subtitle: Text("Edit Your Account"),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("hashdevelop2k20@gmail.com"),
+              trailing: Icon(Icons.send),
+            ),
+            ListTile(
+              leading: Icon(Icons.logout_sharp),
+              title: Text("Logout"),
+              subtitle: Text("Click here to logout"),
+              trailing: Icon(Icons.exit_to_app),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.refresh),
       ),
     );
   }
