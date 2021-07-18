e8b34b67bb08cb7bec502b995120adaaf0f999fa
usman87626 <usman.arif20188@gmail.com>
2021-07-18 17:03:31 +0500

    Chapter 8 Completed

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
-import '../change_name_card.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import '../drawer.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,28 +10,46 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var myText = "Change Me";
+  var url = Uri.parse("https://jsonplaceholder.typicode.com/photos");
+  var data;
+
   TextEditingController _nameController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
+    getData();
+  }
+
+  getData() async {
+    var res = await http.get(url);
+    data = jsonDecode(res.body);
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text("Application With Drawer"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Card(
-            child:
-                ChangeNameCard(myText: myText, nameController: _nameController),
-          ),
-        ),
+        child: data != null
+            ? ListView.builder(
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ListTile(
+                      title: Text(data[index]['title']),
+                      subtitle: Text("ID: ${data[index]["id"]}"),
+                      leading: Image.network(data[index]["url"]),
+                    ),
+                  );
+                },
+              )
+            : Center(child: CircularProgressIndicator()),
       ),
       drawer: MyDrawer(),
       floatingActionButton: FloatingActionButton(
