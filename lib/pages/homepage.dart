e9a72cc69cadd63e7b549deeebf007efd216e88a
usman87626 <usman.arif20188@gmail.com>
2021-07-19 18:06:26 +0500

    Chapter 10 Done

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -9,22 +9,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var myText = "Change Me";
   var url = Uri.parse("https://jsonplaceholder.typicode.com/photos");
   var data;
 
-  TextEditingController _nameController = TextEditingController();
+  // TextEditingController _nameController = TextEditingController();
 
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
-  getData() async {
+  Future getData() async {
     var res = await http.get(url);
     data = jsonDecode(res.body);
-    setState(() {});
+    return data;
   }
 
   @override
@@ -34,28 +27,41 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("Getting Data from API"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: data != null
-            ? ListView.builder(
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: ListTile(
-                      title: Text(data[index]['title']),
-                      subtitle: Text("ID: ${data[index]["id"]}"),
-                      leading: Image.network(data[index]["url"]),
-                    ),
-                  );
-                },
-              )
-            : Center(child: CircularProgressIndicator()),
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return Center(
+                child: Text("Fetch Something"),
+              );
+            case ConnectionState.active:
+            case ConnectionState.waiting:
+              return Center(child: CircularProgressIndicator());
+            case ConnectionState.done:
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text("Something Went Wrong"),
+                );
+              }
+
+              return ListView.builder(itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: ListTile(
+                    title: Text(data[index]["title"]),
+                    subtitle: Text("ID: ${data[index]["id"]}"),
+                    leading: Image.network(data[index]["url"]),
+                  ),
+                );
+              });
+          }
+        },
       ),
       drawer: MyDrawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          myText = _nameController.text;
-          setState(() {});
+          // setState(() {});
         },
         child: Icon(Icons.refresh),
       ),
